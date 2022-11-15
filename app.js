9276b1574dbb208358f3ed96fa5e808ace118077
naekit <mykyta.onishchuck@code.berlin>
2022-11-14 20:03:33 -0500

    initial project structure

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+
+const connectDb = require('./config/db')
+
+// load config
+dotenv.config({ path: './config/config.env' })
+connectDb()
+
+const app = express()
+// logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// handlebars - set view engine
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+const PORT = process.env.PORT || 8080
+
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
