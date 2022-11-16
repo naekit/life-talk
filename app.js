e1ebd245d0d1d773cf126166e4ab35b92ba28b4b
naekit <mykyta.onishchuck@code.berlin>
2022-11-16 09:10:16 -0500

    finished features for adding journal entries

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const MongoStore = require('connect-mongo')
 
 const connectDb = require('./config/db')
@@ -23,6 +24,18 @@ const app = express()
 // body parser
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+
+// method override middleware
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+}))
+
 
 // logging
 if(process.env.NODE_ENV === 'development'){
--- routes/stories.js
+++ routes/stories.js
@@ -40,25 +40,108 @@ router.get('/', ensureAuth, async (req,res) => {
     }
 })
 
+// @desc show 1 story
+// @route GET /stories/:id
+router.get('/:id', ensureAuth, async (req,res) => {
+    try {
+        const story = await Story
+            .findById(req.params.id)
+            .populate('user')
+            .lean()
+        if(!story){
+            return res.render('error/404')
+        }
+        res.render('stories/show', {
+            story
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
+
 // @desc show edit page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req,res) => {
-    const story = await Story.findOne({
-        _id: req.params.id
-    }).lean()
-
-    if(!story) {
-        return res.render('error/404')
+    try{
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean()
+    
+        if(!story) {
+            return res.render('error/404')
+        }
+    
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', {
+                story,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
     }
-
-    if(story.user != req.user.id){
-        res.redirect('/stories')
-    } else {
-        res.render('stories/edit', {
-            story,
-        })
-    }
-
 })
+
+// @desc update story
+// @route PUT /stories/:id
+router.put('/:id', ensureAuth, async (req,res) => {
+    try{ 
+        let story = await Story.findById(req.params.id).lean()
+    
+        if(!story){
+            res.render('error/404')
+        }
+        if(story.user != req.user.id){
+            res.redirect('/stories')
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+        }
+        res.redirect('/dashboard')
+
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+// @desc delete story
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req,res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+// @desc show user stories
+// @route GET /stories/user/:id
+router.get('/user/:userId', ensureAuth, async (req,res) => {
+    try {
+        const stories = await Story.find({ 
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+
+        res.render('stories/index', {
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
 
 module.exports = router
